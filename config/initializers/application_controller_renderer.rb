d18e4445fd8d3ef7c1a57ab32b4dd1c8f8c39351
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-05-12 15:00:39 +0000

    [4-3]finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '4f6398128b9e873f119c334b48bed69ea6d0a846d4dc942c2f9550f3415826114e5b1ea6a22560ebb5b64b469c7c141f7589301d93e5ac28648faf26d9256e0c'
